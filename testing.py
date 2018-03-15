34139fa48c6ca67102f3e43a1fa4a3c210728e90
atc2136 <36896459+atc2136@users.noreply.github.com>
2018-03-14 22:08:24 -0400

    Update testing.py

--- testing.py
+++ testing.py
@@ -158,41 +158,4 @@ import os
 
 e1 = cv2.getTickCount()
 
-# Load two images
-img1 = cv2.imread('key.jpg')
-img2 = cv2.imread('opencv-logo2.png')
-
-#print(img2.shape)
-# I want to put logo on top-left corner, So I create a ROI
-rows,cols,channels = img2.shape
-roi = img1[0:rows, 0:cols ]
-#print(" rows of img2 " , rows , " cols of img2 " , cols , " channels of img2 " , channels)
-# Now create a mask of logo and create its inverse mask also
-img2gray = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
-ret, mask = cv2.threshold(img2gray, 150, 255, cv2.THRESH_BINARY)
-#print("ret is " , ret)
-#print("mask is " , mask)
-mask_inv = cv2.bitwise_not(mask)
-
-# Now black-out the area of logo in ROI
-img1_bg = cv2.bitwise_and(roi,roi,mask = mask)
-
-# Take only region of logo from logo image.
-img2_fg = cv2.bitwise_and(img2,img2,mask = mask_inv)
-
-# Put logo in ROI and modify the main image
-dst = cv2.add(img1_bg,img2_fg)
-img1[0:rows, 0:cols ] = dst
-
-cv2.imshow('img2gray', img2gray)
-cv2.imshow('?', mask)
-cv2.imshow('what', mask_inv)
-cv2.imshow('chuck', img1_bg)
-cv2.imshow('ok', img2_fg)
-cv2.imshow('res',img1)
-e2 = cv2.getTickCount()
-t = (e2 - e1)/cv2.getTickFrequency()
-print(t)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
 
